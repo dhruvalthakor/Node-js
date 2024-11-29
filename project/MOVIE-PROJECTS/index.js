2c12dee1e6d81e30b9aa40f8456c21f80756c47f
dhruvalthakor <dhruvalthakor007@gmail.com>
2024-11-29 12:17:24 +0530

    backend

--- project/MOVIE-PROJECTS/index.js
+++ project/MOVIE-PROJECTS/index.js
@@ -26,50 +26,6 @@ app.get("/admin",(req,res)=>{
     app.use("/", adminRouter);
 
 
-    // app.get("/deleteData/:id", async (req, res) => {
-    //     // console.log(req.params.id);
-    //     try {
-    //       const getDataUser = await bookModel.findById(req.params.id);
-    //       console.log(getDataUser);
-    //       if (getDataUser) {
-    //         fs.unlinkSync(path.join(__dirname, getDataUser.image));
-            
-    //       }
-    //       await bookModel.findByIdAndDelete(req.params.id);
-    //       console.log("Data deleted successfully");
-    //       res.redirect("back");
-    //     } catch (err) {
-    //       console.log(err);
-    //     }
-    //   });
-
-
-      app.get("/editData/:id", async (req, res) => {
-        const storeData = await bookModel.findById(req.params.id);
-        console.log(storeData);
-        res.render("update", { storeData });
-      });
-      
-    
-      app.post("/updateData/:id", bookModel.imageUpload, async (req, res) => {
-        console.log(req.params.id);
-        console.log(req.body);
-        console.log(req.file);
-        try {
-          if (req.file) {
-            const getDataUser = await bookModel.findById(req.params.id);
-            fs.unlinkSync(path.join(__dirname, getDataUser.image));
-          }
-          req.body.image = bookModel.imagePath + "/" + req.file.filename;
-          await bookModel.findByIdAndUpdate(req.params.id, req.body);
-          console.log("Data updated successfully");
-          res.redirect("/");
-        } catch (err) {
-          console.log(err);
-        }
-      });
-
-
 
 app.listen(port,(error)=>{
     if (error) {
