5742f5e34be0c457e342f6b7ba5bb967861932db
dhruvalthakor <dhruvalthakor007@gmail.com>
2024-12-18 09:58:48 +0530

    backend

--- practice/adminpanel/router/dashbordrouter.js
+++ practice/adminpanel/router/dashbordrouter.js
@@ -6,6 +6,9 @@ const passport = require("../cofig/passport-local");
 const SubCategoryModel = require("../model/subCategory");
 const CategoryModel = require("../model/CategoryModel");
 const extraCategoryModel = require("../model/extraCategory");
+const productModel = require("../model/productModel");
+const SubproductModel = require("../model/subproduct");
+const extraproductModel = require("../model/extraproduct");
 const dashbordrouter = express.Router()
 
 dashbordrouter.get("/", async (req, res) => {
@@ -184,5 +187,69 @@ dashbordrouter.get("/addCategory", (req, res) => {
      res.render("viewextraCategory",{getData})
    })
 
+// product 
+dashbordrouter.get("/addproduct", (req, res) => {
+  res.render("addproduct");
+});
+
+dashbordrouter.post("/insertproduct", async (req, res) => {
+  try {
+    console.log(req.body);
+    await productModel.create(req.body);
+    console.log("product created");
+    res.redirect("/addSubproduct");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+dashbordrouter.get("/addSubproduct", async (req, res) => {
+  try {
+    const productes = await productModel.find({});
+    res.render("addSubproduct", { productes: productes });
+  } catch (err) {
+    console.log(err);
+
+  }
+});
+
+dashbordrouter.post("/insertSubproduct", async (req, res) => {
+  try {
+    await SubproductModel.create(req.body);
+    console.log("Subcategory created");
+    res.redirect("/addextraproduct")
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+dashbordrouter.get("/addextraproduct", async (req, res) => {
+  console.log("extra");
+
+  try {
+    const productes = await productModel.find({});
+    const subproductes = await SubproductModel.find({});
+    res.render("addextraproduct", { productes: productes ,subproductes:subproductes});
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+dashbordrouter.post("/insertextraproduct", async (req, res) => {
+  try {
+    await extraproductModel.create(req.body);
+    console.log("extracategory created");
+    res.redirect("/viewextraproduct")
+
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+dashbordrouter.get("/viewextraproduct", async (req, res) => {
+  
+  let getData = await extraproductModel.find().populate("productId").populate("subproductId").exec();
+   res.render("viewextraproduct",{getData})
+ })
 
 module.exports = dashbordrouter
